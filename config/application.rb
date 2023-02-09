8a8c86335bf8e29cedd5a7fc48fe5933351b1e58
Murilo Romeo <muriloh65@gmail.com>
2023-02-09 01:21:55 -0300

    removing useless comments

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails'
 require 'active_model/railtie'
 require 'active_record/railtie'
 require 'active_storage/engine'
+require 'action_cable/engine'
 require 'action_controller/railtie'
 require 'action_mailer/railtie'
 
