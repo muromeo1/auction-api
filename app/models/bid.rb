d2aa9f59857617908c5a92179f038533b2f1e9f9
Murilo Romeo <muriloh65@gmail.com>
2023-02-10 09:24:50 -0300

    added rule to bid not be equal highest

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -10,13 +10,18 @@ class Bid < ApplicationRecord
   def previous
     errors.add(:bid, "can't be from same user consecutively") if from_same_user?
     errors.add(:amount, "can't be lower than the highest bid") if lower_than_highest?
+    errors.add(:amount, "can't be equal the highest bid") if equal_than_highest?
   end
 
   def from_same_user?
-    user_id == Bid.last.user_id
+    user_id == Bid.last&.user_id
   end
 
   def lower_than_highest?
-    amount < Bid.last.amount
+    amount < Bid.last&.amount
+  end
+
+  def equal_than_highest?
+    amount == Bid.last&.amount
   end
 end
--- spec/models/bid_spec.rb
+++ spec/models/bid_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe Bid do
 
   let(:errors) { model.errors.messages }
   let(:user) { create(:user) }
+  let(:user2) { create(:user) }
 
   let(:bid) { build(:bid, user: user) }
-  let(:bid2) { build(:bid, user: user) }
+  let(:bid2) { build(:bid, user: user, amount: 20) }
+  let(:bid3) { build(:bid, user: user2, amount: 20) }
 
   describe 'validation' do
     context 'when adds a bid with blank data' do
@@ -21,6 +23,12 @@ RSpec.describe Bid do
       before { bid.save && bid2.save }
 
       it { expect(bid2.errors.messages[:bid].first).to eq("can't be from same user consecutively") }
+    end
+
+    context 'when new bid is the same amount of highest' do
+      before { bid2.save && bid3.save }
+
+      it { expect(bid3.errors.messages[:amount].first).to eq("can't be equal the highest bid") }
     end
   end
 end
